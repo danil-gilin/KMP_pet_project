18fe88ec1442ba1a33e9db10cf0373627fccd1ab
Даниил Гилин <daniilgilin@MacBook-Pro-Daniil.local>
2025-08-07 16:38:04 +0300

    Добавление календаря

--- shared/src/commonMain/kotlin/info/javaway/spend_sense/di/modules/ViewModelsModule.kt
+++ shared/src/commonMain/kotlin/info/javaway/spend_sense/di/modules/ViewModelsModule.kt
@@ -1,5 +1,6 @@
 package info.javaway.spend_sense.di.modules
 
+import info.javaway.spend_sense.common.ui.calendar.DatePickerViewModel
 import info.javaway.spend_sense.presenter.settings.SettingsViewModel
 import info.javaway.spend_sense.root.RootViewModel
 import org.koin.dsl.module
@@ -7,6 +8,7 @@ import org.koin.dsl.module
 object ViewModelsModule {
     val viewModels = module {
         single { RootViewModel(get()) }
+        single { DatePickerViewModel() }
         factory { SettingsViewModel(get(), get()) }
     }
 }
--- shared/src/commonMain/kotlin/info/javaway/spend_sense/events/EventsScreen.kt
+++ shared/src/commonMain/kotlin/info/javaway/spend_sense/events/EventsScreen.kt
@@ -1,10 +1,23 @@
 package info.javaway.spend_sense.events
 
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import info.javaway.spend_sense.common.ui.calendar.compose.CalendarColors
+import info.javaway.spend_sense.common.ui.calendar.compose.DatePickerView
+import info.javaway.spend_sense.common.ui.theme.AppThemeProvider
+import info.javaway.spend_sense.di.getKoinInstance
 
 @Composable
 fun EventsScreen(modifier: Modifier = Modifier) {
-    Text("Events", modifier = modifier)
+    DatePickerView(
+        viewModel = getKoinInstance(),
+        colors = CalendarColors.default.copy(
+            colorSurface = AppThemeProvider.colors.surface,
+            colorOnSurface = AppThemeProvider.colors.onSurface,
+            colorAccent = AppThemeProvider.colors.accent
+        ),
+        firstDayIsMonday = AppThemeProvider.appPrefs.firstDayIsMonday,
+        labels = emptyList(),
+        selectDayListener = { day -> }
+    )
 }
